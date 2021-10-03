8c301440b4cdfaec3de038924743c4ed7a5b8af5
Emile Loosveld <emile.loosveld@sgsintpaulus.eu>
2021-10-03 14:58:17 +0200

    project deel 1 zo goed als afgewerkt

--- Project fietscomptuer/frminstellingen.Designer.cs
+++ Project fietscomptuer/frminstellingen.Designer.cs
@@ -30,36 +30,172 @@ namespace Project_fietscomptuer
         private void InitializeComponent()
         {
             this.pnlInfo = new System.Windows.Forms.Panel();
+            this.numWielLengte = new System.Windows.Forms.NumericUpDown();
+            this.numAfstand = new System.Windows.Forms.NumericUpDown();
+            this.btnTestRenner = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnlInstellingen = new System.Windows.Forms.Panel();
-            this.pnlKaart = new System.Windows.Forms.Panel();
             this.pnlPoort = new System.Windows.Forms.Panel();
-            this.picKaart = new System.Windows.Forms.PictureBox();
-            this.lblStart = new System.Windows.Forms.Label();
-            this.lblFinish = new System.Windows.Forms.Label();
+            this.pnlKaart = new System.Windows.Forms.Panel();
             this.picFiets = new System.Windows.Forms.PictureBox();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.picKaart = new System.Windows.Forms.PictureBox();
+            this.pnlInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numWielLengte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAfstand)).BeginInit();
             this.pnlInstellingen.SuspendLayout();
             this.pnlKaart.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picKaart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picFiets)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKaart)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlInfo
             // 
+            this.pnlInfo.Controls.Add(this.numWielLengte);
+            this.pnlInfo.Controls.Add(this.numAfstand);
+            this.pnlInfo.Controls.Add(this.btnTestRenner);
+            this.pnlInfo.Controls.Add(this.label4);
+            this.pnlInfo.Controls.Add(this.label3);
+            this.pnlInfo.Controls.Add(this.label2);
+            this.pnlInfo.Controls.Add(this.label1);
             this.pnlInfo.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlInfo.Location = new System.Drawing.Point(0, 0);
+            this.pnlInfo.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pnlInfo.Name = "pnlInfo";
-            this.pnlInfo.Size = new System.Drawing.Size(1350, 148);
+            this.pnlInfo.Size = new System.Drawing.Size(2475, 273);
             this.pnlInfo.TabIndex = 0;
+            // 
+            // numWielLengte
+            // 
+            this.numWielLengte.DecimalPlaces = 3;
+            this.numWielLengte.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numWielLengte.Location = new System.Drawing.Point(928, 105);
+            this.numWielLengte.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numWielLengte.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numWielLengte.Name = "numWielLengte";
+            this.numWielLengte.Size = new System.Drawing.Size(95, 29);
+            this.numWielLengte.TabIndex = 6;
+            this.numWielLengte.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numWielLengte.Value = new decimal(new int[] {
+            2175,
+            0,
+            0,
+            196608});
+            this.numWielLengte.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Num_Increment);
+            this.numWielLengte.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Num_Increment);
+            this.numWielLengte.Leave += new System.EventHandler(this.num_ValueChanged);
+            // 
+            // numAfstand
+            // 
+            this.numAfstand.DecimalPlaces = 1;
+            this.numAfstand.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numAfstand.Location = new System.Drawing.Point(312, 105);
+            this.numAfstand.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numAfstand.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numAfstand.Name = "numAfstand";
+            this.numAfstand.Size = new System.Drawing.Size(95, 29);
+            this.numAfstand.TabIndex = 5;
+            this.numAfstand.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numAfstand.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numAfstand.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Num_Increment);
+            this.numAfstand.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Num_Increment);
+            this.numAfstand.Leave += new System.EventHandler(this.num_ValueChanged);
+            // 
+            // btnTestRenner
+            // 
+            this.btnTestRenner.Location = new System.Drawing.Point(1156, 105);
+            this.btnTestRenner.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTestRenner.Name = "btnTestRenner";
+            this.btnTestRenner.Size = new System.Drawing.Size(179, 99);
+            this.btnTestRenner.TabIndex = 4;
+            this.btnTestRenner.Text = "Test Renner met Fietswiel";
+            this.btnTestRenner.UseVisualStyleBackColor = true;
+            this.btnTestRenner.Click += new System.EventHandler(this.btnTestRenner_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1031, 108);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "m";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(777, 108);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "lengte fietswiel:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "km";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "afstand rit:";
             // 
             // pnlInstellingen
             // 
             this.pnlInstellingen.Controls.Add(this.pnlPoort);
             this.pnlInstellingen.Controls.Add(this.pnlKaart);
             this.pnlInstellingen.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlInstellingen.Location = new System.Drawing.Point(0, 148);
+            this.pnlInstellingen.Location = new System.Drawing.Point(0, 273);
+            this.pnlInstellingen.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pnlInstellingen.Name = "pnlInstellingen";
-            this.pnlInstellingen.Size = new System.Drawing.Size(1350, 506);
+            this.pnlInstellingen.Size = new System.Drawing.Size(2475, 935);
             this.pnlInstellingen.TabIndex = 2;
+            // 
+            // pnlPoort
+            // 
+            this.pnlPoort.AutoSize = true;
+            this.pnlPoort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPoort.Location = new System.Drawing.Point(1833, 0);
+            this.pnlPoort.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pnlPoort.Name = "pnlPoort";
+            this.pnlPoort.Size = new System.Drawing.Size(642, 935);
+            this.pnlPoort.TabIndex = 1;
             // 
             // pnlKaart
             // 
@@ -69,86 +205,87 @@ namespace Project_fietscomptuer
             this.pnlKaart.Controls.Add(this.picKaart);
             this.pnlKaart.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlKaart.Location = new System.Drawing.Point(0, 0);
+            this.pnlKaart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pnlKaart.Name = "pnlKaart";
-            this.pnlKaart.Size = new System.Drawing.Size(1000, 506);
+            this.pnlKaart.Size = new System.Drawing.Size(1833, 935);
             this.pnlKaart.TabIndex = 0;
             // 
-            // pnlPoort
+            // picFiets
             // 
-            this.pnlPoort.AutoSize = true;
-            this.pnlPoort.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlPoort.Location = new System.Drawing.Point(1000, 0);
-            this.pnlPoort.Name = "pnlPoort";
-            this.pnlPoort.Size = new System.Drawing.Size(350, 506);
-            this.pnlPoort.TabIndex = 1;
-            // 
-            // picKaart
-            // 
-            this.picKaart.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.picKaart.Image = global::Project_fietscomptuer.Properties.Resources.defaultkaart;
-            this.picKaart.Location = new System.Drawing.Point(0, 0);
-            this.picKaart.Name = "picKaart";
-            this.picKaart.Size = new System.Drawing.Size(1000, 506);
-            this.picKaart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picKaart.TabIndex = 0;
-            this.picKaart.TabStop = false;
-            // 
-            // lblStart
-            // 
-            this.lblStart.BackColor = System.Drawing.Color.Transparent;
-            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStart.Image = global::Project_fietscomptuer.Properties.Resources.CirkelGroen;
-            this.lblStart.Location = new System.Drawing.Point(38, 55);
-            this.lblStart.Name = "lblStart";
-            this.lblStart.Size = new System.Drawing.Size(43, 35);
-            this.lblStart.TabIndex = 1;
-            this.lblStart.Text = "Start";
-            this.lblStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblStart.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblStart_MouseDown);
+            this.picFiets.BackColor = System.Drawing.Color.Transparent;
+            this.picFiets.Image = global::Project_fietscomptuer.Properties.Resources.rennerL;
+            this.picFiets.Location = new System.Drawing.Point(227, 86);
+            this.picFiets.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.picFiets.Name = "picFiets";
+            this.picFiets.Size = new System.Drawing.Size(138, 98);
+            this.picFiets.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFiets.TabIndex = 3;
+            this.picFiets.TabStop = false;
             // 
             // lblFinish
             // 
             this.lblFinish.BackColor = System.Drawing.Color.Transparent;
             this.lblFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFinish.Image = global::Project_fietscomptuer.Properties.Resources.CirkelRood;
-            this.lblFinish.Location = new System.Drawing.Point(247, 55);
+            this.lblFinish.Location = new System.Drawing.Point(452, 102);
+            this.lblFinish.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblFinish.Name = "lblFinish";
-            this.lblFinish.Size = new System.Drawing.Size(43, 35);
+            this.lblFinish.Size = new System.Drawing.Size(78, 64);
             this.lblFinish.TabIndex = 2;
             this.lblFinish.Text = "Finish";
             this.lblFinish.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblFinish.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblStart_MouseDown);
+            this.lblFinish.MouseMove += new System.Windows.Forms.MouseEventHandler(this.startFinish_MouseMove);
             // 
-            // picFiets
+            // lblStart
             // 
-            this.picFiets.BackColor = System.Drawing.Color.Transparent;
-            this.picFiets.Image = global::Project_fietscomptuer.Properties.Resources.rennerL;
-            this.picFiets.Location = new System.Drawing.Point(124, 46);
-            this.picFiets.Name = "picFiets";
-            this.picFiets.Size = new System.Drawing.Size(75, 53);
-            this.picFiets.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picFiets.TabIndex = 3;
-            this.picFiets.TabStop = false;
+            this.lblStart.BackColor = System.Drawing.Color.Transparent;
+            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Image = global::Project_fietscomptuer.Properties.Resources.CirkelGroen;
+            this.lblStart.Location = new System.Drawing.Point(70, 102);
+            this.lblStart.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(78, 64);
+            this.lblStart.TabIndex = 1;
+            this.lblStart.Text = "Start";
+            this.lblStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblStart.MouseMove += new System.Windows.Forms.MouseEventHandler(this.startFinish_MouseMove);
+            // 
+            // picKaart
+            // 
+            this.picKaart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picKaart.Image = global::Project_fietscomptuer.Properties.Resources.defaultkaart;
+            this.picKaart.Location = new System.Drawing.Point(0, 0);
+            this.picKaart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.picKaart.Name = "picKaart";
+            this.picKaart.Size = new System.Drawing.Size(1833, 935);
+            this.picKaart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picKaart.TabIndex = 0;
+            this.picKaart.TabStop = false;
             // 
             // frminstellingen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1350, 654);
+            this.ClientSize = new System.Drawing.Size(2475, 1208);
             this.Controls.Add(this.pnlInstellingen);
             this.Controls.Add(this.pnlInfo);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.Name = "frminstellingen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "instellingen";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frminstellingen_Load);
+            this.pnlInfo.ResumeLayout(false);
+            this.pnlInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numWielLengte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAfstand)).EndInit();
             this.pnlInstellingen.ResumeLayout(false);
             this.pnlInstellingen.PerformLayout();
             this.pnlKaart.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.picKaart)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picFiets)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKaart)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -163,5 +300,12 @@ namespace Project_fietscomptuer
         private System.Windows.Forms.PictureBox picFiets;
         private System.Windows.Forms.Label lblFinish;
         private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.NumericUpDown numWielLengte;
+        private System.Windows.Forms.NumericUpDown numAfstand;
+        private System.Windows.Forms.Button btnTestRenner;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Project fietscomptuer/frminstellingen.cs
+++ Project fietscomptuer/frminstellingen.cs
@@ -12,6 +12,8 @@ namespace Project_fietscomptuer
 {
     public partial class frminstellingen : Form
     {
+        float ritAfstand = 10000, wiellengte = (float)2.175;
+        float beweegLeft = 0, beweegTop = 0, ritAfgelegdeAfstand = 0;
         public frminstellingen()
         {
             InitializeComponent();
@@ -26,13 +28,105 @@ namespace Project_fietscomptuer
             lblStart.Parent = picKaart;
         }
 
-        private void lblStart_MouseDown(object sender, MouseEventArgs e)
+        private void Num_Increment(object sender, KeyEventArgs e)
         {
-            Point point = picKaart.PointToClient(Cursor.Position);
-            if (point != lblStart.Location)
+            if (sender == numWielLengte)
             {
-                lblStart.Location = point;
+                if (e.Control == true) numWielLengte.Increment = 0.01M;
+                else if (e.Shift == true) numWielLengte.Increment = 0.1M;
+                else if (e.Alt == true) numWielLengte.Increment = 1;
+                else numWielLengte.Increment = 0.001M;
+            }
+            if (sender == numAfstand)
+            {
+                if (e.Control == true) numAfstand.Increment = 1;
+                else if (e.Shift == true) numAfstand.Increment = 10;
+                else if (e.Alt == true) numAfstand.Increment = 100;
+                else numAfstand.Increment = 0.1M;
             }
         }
-    }
+
+        private void num_ValueChanged(object sender, EventArgs e)
+        {
+            if (sender == numAfstand)
+            {
+                ritAfstand = (float)numAfstand.Value * 1000;
+            }
+            if (sender == numWielLengte)
+            {
+                wiellengte = (float)numWielLengte.Value;
+            }
+            ritAfgelegdeAfstand = 0;
+        }
+
+        private void btnTestRenner_Click(object sender, EventArgs e)
+        {
+            //Fietser in de juiste richting laten kijken afhangelijk van zijn x-positie (=left)
+            if (lblStart.Left < lblFinish.Left)
+            {
+                picFiets.Image = Properties.Resources.rennerL;
+            }
+            else
+            {
+                picFiets.Image = Properties.Resources.rennerR;
+            }
+            //fietster alleen bewegen als geheel getal bereikt werd
+            //horizontale beweging
+            //Zorgen dat je komma-getal bijhoudt
+            beweegLeft += ((lblStart.Left - lblFinish.Left) / ritAfstand) * wiellengte;
+            picFiets.Left -= (int)beweegLeft;       // alleen geheel gedeelte wordt aangepast
+            //geheel getal verwijderen want dit werd bijgeteld
+            if (beweegLeft >= 1 || beweegLeft <= -1)
+            {
+                beweegLeft -= (int)beweegLeft;
+            }
+            //verticale beweging
+            //zorgen dat je komma getal bijhoudt
+            beweegTop += ((lblStart.Top - lblFinish.Top) / ritAfstand) * wiellengte;
+            picFiets.Top -= (int)beweegTop;         //alleen geheel gedeelte wordt aangepast
+            //geheel getal verwijderen want dit werd bijgeteld
+            if (beweegTop >= 1 || beweegTop <= -1)
+            {
+                beweegTop -= (int)beweegTop;
+            }
+            //kijken als finish reeds bereikt werd
+            if (ritAfgelegdeAfstand == 0)
+            {
+                picFiets.Left = lblStart.Left;
+                picFiets.Top = lblStart.Top;
+                beweegTop = 0; beweegLeft = 0;
+            }
+            //afstand aanpassen
+            if (ritAfgelegdeAfstand < ritAfstand)
+            {
+                //fietstersafstand bijhouden zodat je weet als hoe ver hij zit
+                ritAfgelegdeAfstand += wiellengte;
+            }
+            else
+            {
+                //fietser op finsh zetten, hij gaat er niet over
+                picFiets.Left = lblFinish.Left;
+                picFiets.Top = lblFinish.Top;
+                //af te leggen afstand terug op 0 om volgende keer terug te kunnen starten
+                ritAfgelegdeAfstand = 0;
+            }
+        }
+
+        private void startFinish_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point point = picKaart.PointToClient(Cursor.Position);
+            if (e.Button == MouseButtons.Left && point != lblStart.Location && sender == lblStart)
+            {
+                lblStart.Top = point.Y - Cursor.Size.Height / 2;
+                lblStart.Left = point.X - Cursor.Size.Width / 2;
+                ritAfgelegdeAfstand = 0;
+            }
+            if (e.Button == MouseButtons.Left && point != lblFinish.Location && sender == lblFinish)
+            {
+                lblFinish.Top = point.Y - Cursor.Size.Height / 2;
+                lblFinish.Left = point.X - Cursor.Size.Width / 2;
+                ritAfgelegdeAfstand = 0;
+            }
+        }
+    }    
 }
